5b3c6547d4de091f164d274bfd469a99857150f2
Parsa Hashemkhany <parsahashemkhany.1385.pppp@gmail.com>
2024-10-11 04:13:04 -0700

    some new html files and changes

--- FlaskWebsite2/app.py
+++ FlaskWebsite2/app.py
@@ -3,8 +3,27 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
+@app.route('/home')
 def home_page():
-    return render_template('home.html')
+    return render_template('Base.html')
+
+
+@app.route('/market')
+def market_page():
+    items: list[dict] = [
+        {'name': 'Product 1',
+         'description': 'This is a great product that you will love. It has excellent features and is available at an amazing price.',
+         'price': 20},
+
+        {'name': 'Product 2',
+         'description': 'This is a great product that you will love. It has excellent features and is available at an amazing price.',
+         'price': 50},
+
+        {'name': 'Product 3',
+         'description': 'This is a great product that you will love. It has excellent features and is available at an amazing price.',
+         'price': 150}
+    ]
+    return render_template('Market.html', items=items)
 
 if __name__ == '__main__':
     app.run(debug=True)
